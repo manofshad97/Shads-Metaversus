cd5db6eb7b1574535d5f1813b11690102c7d97a1
Shadman Ahmed <travisislit2345@gmail.com>
2022-12-13 15:09:02 -0500

    added eslintignore

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-import styles from '../styles';
-import { navVariants } from '../utils/motion';
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
 
 const Navbar = () => (
   //outer nav element
@@ -16,18 +16,17 @@ const Navbar = () => (
     {/*container div for gradient side of navbar */}
     <div className="absolute w-[50%] inset-0 gradient-01" />
     {/*outer nav container. split all elements using justify between, gap-8 */}
-    <div
-      className={`${styles.innerWidth} `}
-    >
+    <div className={`${styles.innerWidth} `}>
       {/* <img
         src="/search.svg"
         alt="search"
         className="w-[24px] h-[24px] object-contain"
       /> */}
-      <a href="#about"><h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
-        SHAD'S METAVERSUS
-      </h2></a>
-      
+      <a href="#about">
+        <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
+          SHAD'S METAVERSUS
+        </h2>
+      </a>
       {/* <img
         src="/menu.svg"
         alt="menu"
